273fbd3cb1454a498f68e863a4ee66ab93ab4c2b
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-09-10 14:05:13 +0200

    search logic

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Search @search="search" />
+    <button @click="find">Cerca</button>
+
+    <!-- pseudo card  componente to be -->
+
+    <div class="card" v-for="result in searchResult" :key="result.id">
+      <h3 class="text-dark">{{ result.title }}</h3>
+      <h5>{{ result.original_title }}</h5>
+      <h5>{{ result.original_language }}</h5>
+      <h5>{{ result.vote_average }}</h5>
+      <figure></figure>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import Search from "./components/Search.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Search,
+  },
+  data() {
+    return {
+      searchResult: [],
+      searchText: " ",
+    };
+  },
+
+  // da trasformare in funzione al keyup
+  methods: {
+    find() {
+      const apikey = "cde3eaa50ec9e14e90a124f80a98153d";
+      const baseUri = "https://api.themoviedb.org/3";
+
+      // clean page in search empty
+      if (this.searchText === " " || !this.searchText) {
+        this.searchResult = [];
+      } else {
+        axios
+          .get(
+            `${baseUri}/search/movie?api_key=${apikey}&query=${this.searchText}`
+          )
+          .then((res) => {
+            this.searchResult = res.data.results;
+            console.log(this.searchResult);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    },
+    search(text) {
+      this.searchText = text;
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./scss/style.scss";
 </style>
---
+++ src/components/Search.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input
+      type="text"
+      placeholder="Cerca..."
+      v-model="searchText"
+      @keyup="$emit('search', searchText)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  data() {
+    return {
+      searchText: "",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
